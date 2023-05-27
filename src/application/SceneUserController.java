f09923da7716baa94985fb6f579c7cb4fce17831
gsantamaria83 <gilsaint24@gmail.com>
2023-05-27 18:32:20 -0500

    Cambio sobre labels de mensaje

--- src/application/SceneUserController.java
+++ src/application/SceneUserController.java
@@ -37,22 +37,33 @@ public class SceneUserController implements Initializable {
 	public void SwitchScene(ActionEvent event)throws IOException{
 		String user = txtUser.getText();
 		
-		if(!user.equals("movaccuratelevel6")) {
-			loader = new FXMLLoader(getClass().getResource("SceneHistory.fxml"));
+		if(user.equals("movaccuratelevel6")) {
+			loader = new FXMLLoader(getClass().getResource("Scene7.fxml"));
 			root = loader.load();
-			SceneHistoryController scene5Controller = loader.getController();
+			Scene7Controller scene7Controller = loader.getController();
+			scene7Controller.lblUser.setText(user);
+			scene7Controller.lblScore.setText("7000");
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+		else if(user.equals("movacomplishlevel5")) {
+			loader = new FXMLLoader(getClass().getResource("Scene5.fxml"));
+			root = loader.load();
+			Scene5Controller scene5Controller = loader.getController();
 			scene5Controller.lblUser.setText(user);
+			scene5Controller.lblScore.setText("4000");
 			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
 			stage.setScene(scene);
 			stage.show();
 		}
 		else {
-			loader = new FXMLLoader(getClass().getResource("Scene7.fxml"));
+			loader = new FXMLLoader(getClass().getResource("SceneHistory.fxml"));
 			root = loader.load();
-			Scene7Controller scene7Controller = loader.getController();
-			scene7Controller.lblUser.setText(user);
-			scene7Controller.lblScore.setText("7000");
+			SceneHistoryController scene5Controller = loader.getController();
+			scene5Controller.lblUser.setText(user);
 			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
 			stage.setScene(scene);
